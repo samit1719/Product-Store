cce36dc757f8597ea1b2ca5adc868af8c9204cf0
samit1719 <samuelteshome1719@gmail.com>
2025-02-02 22:14:12 +0300

    a

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -49,11 +49,15 @@ export const updatedProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     const { id } = req.params;
     // console.log("ïd", id);
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({success: false, message: "Invalid product id"});
+    }
+
     try {
       await Product.findByIdAndDelete(id);
       res.status(200).json({success: true , message: "Product deleted successfully"});
     }catch(error) {
       console.log("error in deleting product:", error.message);
-      res.status(404).json({success: false, message: "Product not found"});
+      res.status(500).json({success: false, message: "server error"});
     }
   }
--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,16 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config(); // to use environment variables
 
 const app = express(); // create an express app
+const PORT = process.env.PORT || 5000; // set the port for the server
 
 app.use(express.json()); // to parse json to javascript object from the request body
 app.use("/api/products", productRoutes); // use the product routes for all routes starting with /api/products
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 5000");
+  console.log("Server Started at http://localhost:" + PORT);
 });
+
+
 

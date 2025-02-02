fbdc7b9490fc554374ff0a88a3431b413871a851
samit1719 <samuelteshome1719@gmail.com>
2025-02-02 13:31:41 +0300

    backend 1

---
+++ backend/routes/product.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createProduct, deleteProduct, getProducts, updatedProduct } from '../controllers/product.controller.js';
+
+const router = express.Router();  
+
+  router.post("/", createProduct); // create a new product in the database
+  router.get("/", getProducts); // get all products from the database
+  router.put("/:id", updatedProduct); // update a product in the database
+  router.delete("/:id", deleteProduct); // delete a product from the database
+
+export default router;  
--- backend/server.js
+++ backend/server.js
@@ -2,58 +2,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config(); // to use environment variables
 
 const app = express(); // create an express app
 
 app.use(express.json()); // to parse json to javascript object from the request body
-
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({}); // get all products from the database
-    res.status(200).json({success: true, data: products}); // send the products as response
-  } catch (error) {
-    console.log("error in getting products:", error.message);
-    res.status(500).json({success: false, message: "Server error"});
-  }
-});
-
-app.post("/api/products", async (req, res) =>  {
-  const product = req.body; // req.body is the data that is sent to the server from the client
-
-  if(!product.name || !product.price || !product.image) {
-    return res.status(400).json({success: false, message: "All fields are required"});
-  }
-
-  const newProduct = new Product(product); // create a new product
-  
-  try {
-    await newProduct.save(); // save the product to the database
-    res.status(201).json({success: true, data: newProduct});
-  } catch (error) {
-    console.log("error in creating product:", error.message);
-    res.status(500).json({success: false, message: "Internal server error"});
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  // console.log("ïd", id);
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({success: true , message: "Product deleted successfully"});
-  }catch(error) {
-    console.log("error in deleting product:", error.message);
-    res.status(404).json({success: false, message: "Product not found"});
-  }
-});
-
+app.use("/api/products", productRoutes); // use the product routes for all routes starting with /api/products
 
 
 app.listen(5000, () => {
   connectDB();
   console.log("Server is running on port 5000");
 });
+
